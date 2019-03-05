9223564236090bdfcd4961d499803a810e1fd3e1
Nick Manning <nicholas.manning@gmail.com>
2019-03-04 20:29:25 -0500

    Simple, styled button with no functionality.

--- lib/app.dart
+++ lib/app.dart
@@ -6,6 +6,12 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Web Views',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          fontFamily: "Arial",
+          textTheme: TextTheme(
+              button: TextStyle(color: Colors.white, fontSize: 18.0),
+              title: TextStyle(color: Colors.red))),
       home: Home(),
     );
   }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
+  final _links = ['https://google.com'];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: SafeArea(
+            child: SingleChildScrollView(
+                child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: _links.map((link) => _button(context, link)).toList(),
+    ))));
+  }
+
+  Widget _button(BuildContext context, String url) {
+    return Container(
+        padding: EdgeInsets.all(20.0),
+        child: FlatButton(
+          color: Theme.of(context).primaryColor,
+          padding: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 15.0),
+          child: Text(url),
+          onPressed: () => print('hi'),
+        ));
   }
 }
