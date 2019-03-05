3ea30b514e16cdbabaa5db600787f1dd4f36efd9
Nick Manning <nicholas.manning@gmail.com>
2019-03-05 08:19:09 -0500

    Implements  version of

--- lib/screens/web_view_container.dart
+++ lib/screens/web_view_container.dart
@@ -1,22 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class WebViewContainer extends StatelessWidget {
+class WebViewContainer extends StatefulWidget {
   final url;
-  final _key = UniqueKey();
 
   WebViewContainer(this.url);
+
+  @override
+  createState() => _WebViewContainerState(this.url);
+}
+
+class _WebViewContainerState extends State<WebViewContainer> {
+  var _url;
+  final _key = UniqueKey();
+
+  _WebViewContainerState(this._url);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(),
-        body: Column(children: [
-          Expanded(
-              child: WebView(
-                  key: _key,
-                  javascriptMode: JavascriptMode.unrestricted,
-                  initialUrl: url))
-        ]));
+        body: Column(
+          children: [
+            Expanded(
+                child: WebView(
+                    key: _key,
+                    javascriptMode: JavascriptMode.unrestricted,
+                    initialUrl: _url))
+          ],
+        ));
   }
 }
