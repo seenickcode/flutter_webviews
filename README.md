12e30961fbd29bdfb33c6ff5878e842a62b56366
Nick Manning <nicholas.manning@gmail.com>
2019-03-04 19:41:36 -0500

    Simple app with empty Container for home screen.

---
+++ README.md
@@ -0,0 +1,10 @@
+# flutter_webviews
+
+## Steps
+
+1. Create a boilerplate app with main, app and screens/home.dart.
+
+
+## Resources
+
+- https://pub.dartlang.org/packages/webview_flutter
---
+++ lib/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'screens/home.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Web Views',
+      home: Home(),
+    );
+  }
+}
